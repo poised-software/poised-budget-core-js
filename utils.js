132142d17d9d9c221d7c692171c4a0c5129ab65b
poised-soft <poised@cloakedpotion.com>
2025-03-18 19:33:06 -0500

    More loging....

--- utils.js
+++ utils.js
@@ -36,6 +36,7 @@ export async function getCategories(PouchDB, openpgp, params, password) {
         categoryResults = await categoriesDB.allDocs({ include_docs: true });
     }
     if (!sessionStorage.getItem("decryptedCategories")) {
+        console.log(categoryResults)
         categoryResults = await decryptAllDocs(openpgp, categoryResults.rows, password);
         sessionStorage.setItem("decryptedCategories", JSON.stringify(categoryResults.flatMap(c => c.doc)));
     }
@@ -60,9 +61,7 @@ export function getNextOrPreviousMonth(budgetMonth, next = true) {
 }
 
 export async function decryptAllDocs(openpgp, rows, password) {
-    console.log(rows)
     const flatRows = rows.flatMap(r => r.doc)
-    console.log(flatRows)
     await Promise.all(flatRows.map(async d => await decryptDoc(openpgp, d, password)));
 }
 
